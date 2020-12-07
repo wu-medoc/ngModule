43919081d5c9200954cb4403236615f68ea8e5b1
wu-medoc <58807721+wu-medoc@users.noreply.github.com>
2020-12-07 17:48:44 +0800

    ngModule Demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./entrance/entrance.module').then(m => m.EntranceModule) },
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', loadChildren: () => import('./entrance/entrance.module').then(m => m.EntranceModule) },
   { path: 'ExploreMap', loadChildren: () => import('./explore/explore.module').then(m => m.ExploreModule) },
   { path: 'Function', loadChildren: () => import('./function/function.module').then(m => m.FunctionModule) },
   { path: 'Travel', loadChildren: () => import('./travel/travel.module').then(m => m.TravelModule) },
